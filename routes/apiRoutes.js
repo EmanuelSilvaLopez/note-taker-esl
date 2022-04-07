7a7cefc7421a7d348eba2c23e33f9ca233c16091
EmanSL <emanuelsilvalopez@gmail.com>
2022-04-07 14:12:19 -0400

    added html routes and began apiRoutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const {notes} = require("../Develop/db/db.json")
+
+router.get("/notes", (req, res) => {
+  let results = notes;
+  res.json(results);
+});
+
+router.post("/notes", (req, res) => {
+  
+})
+
+router.delete("/notes/:id", (req, res) => {
+  
+})
