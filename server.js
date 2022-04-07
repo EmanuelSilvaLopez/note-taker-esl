50473b2fe2ede6d8d848f28df6a94e5a266040d9
EmanSL <emanuelsilvalopez@gmail.com>
2022-04-07 08:40:23 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// Parse incoming JSON data
+app.use(express.json());
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server now on http://localhost:${PORT}`);
+});
