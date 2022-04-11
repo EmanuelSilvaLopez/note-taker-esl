95654db34136345a4e81408d776a769d2c4ff898
EmanSL <emanuelsilvalopez@gmail.com>
2022-04-11 17:05:54 -0400

    pushing to main

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 3001
 //Reading static file inside public folders
 app.use(express.static(__dirname + '/public'));
 
-//Body parsor
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
